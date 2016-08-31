2a92bf698a0d20a792d7f5e5190ae5039521045e
Erick Apolo <apoloerick02@gmail.com>
2016-08-31 18:13:22 -0400

    gem collector

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+	var win =0;
+	var losses =0;
+	var randnumber =Math.floor(Math.random*101)+19;
+	var blue =Math.floor(Math.random*11)+1;
+	var red =Math.floor(Math.random*11)+1;
+	var pink =Math.floor(Math.random*11)+1;
+	var green =Math.floor(Math.random*11)+1;
+	var totalscore = 0;
+
+    
+});
